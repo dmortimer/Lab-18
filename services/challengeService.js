6711f3fff8b399ff45d9346572148461ff4dfe8c
Steven Turner <Stevent70899@gmail.com>
2017-05-08 13:31:06 -0400

    bam

--- services/challengeService.js
+++ services/challengeService.js
@@ -5,21 +5,21 @@ app.factory('challengeService', function() {
     return {
         checkNum: function(i) {
 
-            for (var i = 1; i <= 100; i++) {
-              if (((i % 3) === 0) && ((i % 5) === 0)) {
-                console.log("FizzBuzz");
-                var word = "fizzbuzz";
-              } else if ((i % 3) === 0) {
-                console.log("Buzz");
-                var word = "buzz";
-              } else if ((i % 5)=== 0) {
-                console.log("Fizz");
-                var word = "fizz";
-              } else {
-                console.log(i);
-              }
-            }
-
+            // for (var i = 1; i <= 100; i++) {
+            //   if (((i % 3) === 0) && ((i % 5) === 0)) {
+            //     console.log("FizzBuzz");
+            //     var word = "fizzbuzz";
+            //   } else if ((i % 3) === 0) {
+            //     console.log("Buzz");
+            //     var word = "buzz";
+            //   } else if ((i % 5)=== 0) {
+            //     console.log("Fizz");
+            //     var word = "fizz";
+            //   } else {
+            //     console.log(i);
+            //   }
+            // }
+            console.log(i);
 
         }
     }
